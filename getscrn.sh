10c7886742c39a99346560b0f476160d92d4e3d2
Jeff Ratliff <jefrat@gmail.com>
2021-05-25 18:46:30 -0400

    Just some comments and formatting for clarity

--- getscrn.sh
+++ getscrn.sh
@@ -7,14 +7,18 @@
 screens=(`screen -ls | grep -E 'Detached|Attached' | tr [:blank:] _ | cut -d _ -f2-`)
 
 line=1
+# Make a list of sessions to choose from
 for i in "${screens[@]}"
 	do echo $line "$i"
 	line=$(($line + 1))
 done
+
 # Get user choice
 read -p 'Attach which session? ' response
 # Arrays are indexed from zero
 response=$((response - 1))
+
 chosen="${screens[$response]}"
 chosen=`echo ${screens[$response]} | cut -d _ -f1`
+# do the needful
 screen -dRa $chosen
